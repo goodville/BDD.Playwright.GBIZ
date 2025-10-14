d1df9be672c1984efe5ecdc2381dcefdebe19987
ASwathi <ASwathi@RDPDEV4>
2025-10-14 03:40:20 -0400

    Pushing the Changes for Page Elements

---
+++ BDD.Playwright.Origami/PageElements/MenuItem.cs
@@ -0,0 +1,88 @@
+﻿using BDD.Playwright.Origami.PageElements;
+using Microsoft.Playwright;
+using Reqnroll;
+
+namespace BDD.Playwright.Origami.PageElements
+{
+    public class MenuItem(ScenarioContext scenarioContext) : BaseElement(scenarioContext)
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MenuItem"/> class.
+        /// </summary>
+        /// <summary>
+        /// Clicks the menu item.
+        /// </summary>
+        /// <param name="labelnameorlocator">The labelnameorlocator.</param>
+        /// <param name="action">The action.</param>
+        /// <param name="islocator">if set to <c>true</c> [islocator].</param>
+        /// <param name="index">The index.</param>
+        public async Task ClickMenuItemAsync(string labelnameorlocator, ActionType action = ActionType.Click, bool islocator = false, int index = 1)
+        {
+            var element = await GetMenuItemByLabelAsync(labelnameorlocator, islocator, index);
+
+            if (element != null)
+            {
+                await element.ClickAsync();
+                logger.WriteLine(string.Format("Button click performed with value '{0}' on Button field with locator '{1}'", action, labelnameorlocator));
+            }
+        }
+
+        /// <summary>
+        /// Verifies the menu item.
+        /// </summary>
+        /// <param name="labelnameorlocator">The labelnameorlocator.</param>
+        /// <param name="islocator">if set to <c>true</c> [islocator].</param>
+        /// <param name="index">The index.</param>
+        /// <returns>bool value</returns>
+        public async Task <bool> VerifyMenuItemAsync(string labelnameorlocator, bool islocator = false, int index = 1)
+        {
+            var element = await GetMenuItemByLabelAsync(labelnameorlocator, islocator, index);
+
+            if (element != null)
+            {
+                logger.WriteLine(string.Format("'{0}' button exist ", labelnameorlocator));
+                return true;
+            }
+            else
+            {
+                logger.WriteLine(string.Format("'{0}' button does not exist ", labelnameorlocator));
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Gets the menu item by label.
+        /// </summary>
+        /// <param name="labelnameorlocator">The labelnameorlocator.</param>
+        /// <param name="islocator">if set to <c>true</c> [islocator].</param>
+        /// <param name="index">The index.</param>
+        /// <returns>IWebElement Response</returns>
+        private async Task <ILocator> GetMenuItemByLabelAsync(string labelnameorlocator, bool islocator = false, int index = 1)
+        {
+            var XPathLocator = string.Empty;
+            try
+            {
+                XPathLocator = islocator
+                    ? string.Format("({0})[{1}]", labelnameorlocator, index)
+                    : string.Format("(//*[.=normalize-space(\"{0}\")]/parent::div)[{1}]", labelnameorlocator, index);
+                ILocator element;
+                if (_frameLocator != null)
+                {
+                    element = _frameLocator.Locator($"xpath={XPathLocator}");
+                }
+                else
+                {
+                    element = _page.Locator($"xpath={XPathLocator}");
+                }
+
+                await element.WaitForAsync(new LocatorWaitForOptions { State = WaitForSelectorState.Visible });
+                return element;
+            }
+            catch (Exception ex)
+            {
+                logger.WriteLine("Error:Element locator " + labelnameorlocator + " did not match any elements." + ex);
+                return null;
+            }
+        }
+    }
+}
---
+++ BDD.Playwright.Origami/PageElements/RadioButton.cs
@@ -0,0 +1,111 @@
+﻿using BDD.Playwright.Origami.PageElements;
+using Microsoft.Playwright;
+using Reqnroll;
+using System;
+
+namespace BDD.Playwright.Origami.PageElements
+{
+    public class RadioButton(ScenarioContext scenarioContext) : BaseElement(scenarioContext)
+    {
+        public async Task SelectRadioButtonAsync(string labelNameOrLocator, string value, bool isLocator, int index)
+        {
+            var element = await GetRadioButtonByLabelAsync(labelNameOrLocator, isLocator, index, value);
+
+            if (element != null && !await element.IsCheckedAsync())
+            {
+                await element.ClickAsync();
+                logger.WriteLine($"RadioButton Selected with locator '{labelNameOrLocator}'");
+            }
+            else if (element != null && await element.IsCheckedAsync())
+            {
+                logger.WriteLine($"RadioButton with locator '{labelNameOrLocator}' is already selected.");
+            }
+            else
+            {
+                throw new Exception("Error @ RadioButton.SelectRadioButtonAsync. Didn't match any element with locator " + labelNameOrLocator);
+            }
+        }
+
+        public async Task<bool> VerifyRadioButtonSelectionAsync(string labelNameOrLocator, bool expectedResult, bool isLocator = false, int index = 1, string displayName = null, string yesOrNo = "yes")
+        {
+            // Assuming you have a method similar to GetRadioButtonByLabelAsync
+            var element = await GetRadioButtonByLabelAsync(labelNameOrLocator, isLocator, index, yesOrNo);
+
+            var labelDisplayName = displayName ?? labelNameOrLocator;
+
+            if (element == null)
+            {
+                logger.WriteLine($"'{labelDisplayName}' Radio button not found for validation.");
+                return false;
+            }
+
+            var isChecked = await element.IsCheckedAsync();
+
+            if (isChecked == expectedResult)
+            {
+                logger.WriteLine($"'{labelDisplayName}' Radio button validation success");
+                return true;
+            }
+            else
+            {
+                logger.WriteLine($"'{labelDisplayName}' Radio button validation is not success");
+                return false;
+            }
+        }
+
+        public async Task<bool> VerifyRadioButtonExistAsync(string labelNameOrLocator, bool isLocator = false, int index = 1, string displayName = null)
+        {
+            // Try to find either Yes or No radio button for the given label
+            var element = await GetRadioButtonByLabelAsync(labelNameOrLocator, isLocator, index, "yes");
+
+            var labelDisplayName = displayName ?? labelNameOrLocator;
+
+            if (element != null)
+            {
+                logger.WriteLine($"'{labelDisplayName}' Radio button exists.");
+                return true;
+            }
+            else
+            {
+                logger.WriteLine($"'{labelDisplayName}' Radio button does not exist.");
+                return false;
+            }
+        }
+
+        private async Task<ILocator> GetRadioButtonByLabelAsync(string labelNameOrLocator, bool isLocator, int index, string yesOrNo)
+        {
+            string xPathLocator;
+            try
+            {
+                if (isLocator)
+                {
+                    xPathLocator = $"({labelNameOrLocator})[{index}]";
+                }
+                else
+                {
+                    var radioValue = yesOrNo.Equals("yes", StringComparison.OrdinalIgnoreCase) ? "Yes" : "No";
+                    xPathLocator = $"(//div[./div[normalize-space(text())='{labelNameOrLocator}']]//div[@role='radio' and @aria-label='{radioValue}'])[{index}]";
+                    Console.WriteLine(xPathLocator);
+                }
+
+                ILocator element;
+                if (_frameLocator != null)
+                {
+                    element = _frameLocator.Locator(xPathLocator);
+                }
+                else
+                {
+                    element = _page.Locator(xPathLocator);
+                }
+
+                await element.WaitForAsync(new LocatorWaitForOptions { State = WaitForSelectorState.Visible });
+                return element;
+            }
+            catch (Exception err)
+            {
+                logger.WriteLine($"WriteLine: Element locator '{labelNameOrLocator}' did not match any elements.{err.Message}");
+                return null;
+            }
+        }
+    }
+}

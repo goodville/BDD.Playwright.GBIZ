b377f49025785008f33d0d8006a239833df744bc
SDeshmuk <SDeshmuk@RDPDEV4>
2025-10-15 08:01:14 -0400

    Login and Footer page

---
+++ BDD.Playwright.Test/GlobalPages/FooterLinks.cs
@@ -0,0 +1,67 @@
+﻿using GoodVille.GBIZ.Reqnroll.Automation.PageElements;
+using GoodVille.GBIZ.Reqnroll.Automation.Pages.CommonPage;
+using GoodVille.GBIZ.Reqnroll.Automation.Pages.XpathProperties;
+using Microsoft.CodeAnalysis.CSharp;
+using Reqnroll;
+using SeleniumExtras.PageObjects;
+
+namespace BDD.Playwright.Test.GlobalPages
+{
+    public class FooterLink : BasePage
+    {
+        public CommonXpath _commonXpath;
+        public FooterLink(ScenarioContext scenarioContext, CommonXpath commonXpath) : base(scenarioContext)
+        {
+
+            PageFactory.InitElements(Driver, this);
+            _commonXpath = commonXpath;
+
+        }
+        #region Xpath
+        public string ShadowHost_Text => ".gg-footer.hydrated";
+        public string PrivacyPolicy_Link => "div.footernav> a[href='/help/privacypolicy/']";
+        public string ContactUs_Link => "div.footernav> a[href='/contactus/']";
+        public string TermsandConditions_Link => "div.footernav> a[href='/help/terms/']";
+        public string RetrySign_Btn => "//a[contains(text(),'Retry Sign in')]";
+        #endregion
+        public async Task PrivacyMethodAsync()
+        {
+            //commonFunctions.UserWaitForPageToLoadCompletly();
+            Button.ClickButtonCss(ShadowHost_Text, PrivacyPolicy_Link);
+            //Driver.Navigate().Back();
+            commonFunctions.UserWaitForPageToLoadCompletly();
+            RetrySignin();
+        }
+
+        public async Task ContactUsMethodAsync()
+        {
+            //commonFunctions.UserWaitForPageToLoadCompletly();
+            Button.ClickButtonCss(ShadowHost_Text, ContactUs_Link);
+            //Driver.Navigate().Back();
+            //commonFunctions.UserWaitForPageToLoadCompletly();
+            RetrySignin();
+        }
+
+        public async Task TermMethodAsync()
+        {
+            //commonFunctions.UserWaitForPageToLoadCompletly();
+            Button.ClickButtonCss(ShadowHost_Text, TermsandConditions_Link);
+            //Driver.Navigate().Back();
+            //commonFunctions.UserWaitForPageToLoadCompletly();
+            RetrySignin();
+        }
+
+        public async Task RetrySigninAsync()
+        {
+            if (Button.VerifyButtonExistCss(ShadowHost_Text, TermsandConditions_Link))
+            {
+                SpecLogger.WriteLine("Page is Loaded Successfully and signed to Agent Home");
+            }
+            else
+            {
+                Button.ClickButton(RetrySign_Btn, ActionType.Click, true, 1);
+                //commonFunctions.UserWaitForPageToLoadCompletly();
+            }
+        }
+    }
+}
---
+++ BDD.Playwright.Test/GlobalPages/LoginPage.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using GoodVille.GBIZ.Reqnroll.Automation.Helpers;
+using GoodVille.GBIZ.Reqnroll.Automation.PageElements;
+using GoodVille.GBIZ.Reqnroll.Automation.Pages.CommonPage;
+using GoodVille.GBIZ.Reqnroll.Automation.Pages.XpathProperties;
+using Reqnroll;
+using SeleniumExtras.PageObjects;
+
+namespace GoodVille.GBIZ.Reqnroll.Automation.Pages.GlobalPages
+{
+    public class LoginPage : BasePage
+    {
+        //public CommonXpath _commonXpath;
+        private readonly string _scenarioTitle;
+        public LoginPage(ScenarioContext scenarioContext) : base(scenarioContext)
+        {
+
+            PageFactory.InitElements(Driver, this);
+            //_commonXpath = commonXpath;
+            _scenarioTitle = scenarioContext.ScenarioInfo.Title;
+
+        }
+        #region Xpath
+        #endregion
+        public async Task LoginAsync()
+        {
+            commonFunctions.ReadTestDataForLoginPage();
+            commonFunctions.UserWaitForPageToLoadCompletly();
+            if (commonFunctions.AccountName_LabelAndValue.Item2 == "Agents")
+            {
+                Button.ClickButton(_commonXpath.AgentPortal, ActionType.Click, true, 1, "Agents");
+            }
+            else if (commonFunctions.AccountName_LabelAndValue.Item2 == "Members")
+            {
+                Button.ClickButton(_commonXpath.Members, ActionType.Click, true, 1, "Members");
+            }
+            InputField.SetTextAreaInputField(_commonXpath.UsernameInp, commonFunctions.UserName_LabelAndValue.Item2, true, 1, "UserName");
+            InputField.SetTextAreaInputField(_commonXpath.PasswordInp, commonFunctions.Password_LabelAndValue.Item2, true, 1, "Password");
+            Button.ClickButtonForStaleElementWithoutDepen(_commonXpath.SignInBtn, ActionType.Click, true, 1, "Login");
+            Console.WriteLine("User logged into GBIZ");
+            commonFunctions.UserWaitForPageToLoadCompletly();
+        }
+
+        public async Task LoginUsingJSONAsync()
+        {
+            commonFunctions.UserWaitForPageToLoadCompletly();
+            var jsonDataReader = new JsonTestDataReader();
+            var testData = JsonTestDataReader.LoadTestData(_scenarioTitle, "AgentLogin.json");
+            if (testData["Account"].ToString() == "Agents")
+            {
+                Button.ClickButton(_commonXpath.AgentPortal, ActionType.Click, true, 1, "Agents");
+            }
+            else if (testData["Account"].ToString() == "Members")
+            {
+                Button.ClickButton(_commonXpath.Members, ActionType.Click, true, 1, "Members");
+            }
+            InputField.SetTextAreaInputField(_commonXpath.UsernameInp, testData["UserName"].ToString(), true, 1, "UserName");
+            InputField.SetTextAreaInputField(_commonXpath.PasswordInp, testData["Password"].ToString(), true, 1, "Password");
+            Button.ClickButtonForStaleElementWithoutDepen(_commonXpath.SignInBtn, ActionType.Click, true, 1, "Login");
+            Console.WriteLine("User logged into GBIZ");
+            commonFunctions.UserWaitForPageToLoadCompletly();
+        }
+    }
+}

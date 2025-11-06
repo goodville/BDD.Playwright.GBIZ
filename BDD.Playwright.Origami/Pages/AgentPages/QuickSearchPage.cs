09ca65149845aca56931ffde314cbb20a364c1c4
ssurapar <Sekhar.Suraparaju@goodville.com>
2025-11-06 12:02:57 -0500

    Pushing AgentPages_Sekhar

---
+++ BDD.Playwright.Origami/Pages/AgentPages/QuickSearchPage.cs
@@ -0,0 +1,55 @@
+﻿
+using BDD.Playwright.Core.Loggers;
+using BDD.Playwright.GBIZ.Pages.CommonPage;
+using Microsoft.Playwright;
+using Reqnroll;
+
+namespace BDD.Playwright.Origami.Pages.AgentPages
+{
+    public class QuickSearchPage : BasePage
+    {
+        private readonly ScenarioContext _scenarioContext;
+        private readonly ApplicationLogger logger;
+        public QuickSearchPage(ScenarioContext scenarioContext) : base(scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+            logger = scenarioContext.Get<ApplicationLogger>("Logger");
+        }
+
+        #region Xpath
+        public string SearchPolicy_Inp { get; set; } = "//input[@id='search_term']";
+        public string Search_Radio { get; set; } = "//input[@id='search' and @type='radio']";
+        public string Claim_Radio { get; set; } = "//input[@id='claim' and @type='radio']";
+        public string Quote_Radio { get; set; } = "//input[@id='quote' and @type='radio']";
+        public string Continue_Btn { get; set; } = "//input[contains(@value,'Continue')]";
+        public string PolicyClick_Text { get; set; } = "//td[contains(text(),'{0}')]";
+        #endregion
+
+        public async Task QuickSearchMethodAsync(string radio, string input)
+        {
+            // Wait for page to load
+            await page.WaitForLoadStateAsync(LoadState.DOMContentLoaded);
+            // Enter policy number
+            var inputField = page.Locator(SearchPolicy_Inp);
+            await inputField.FillAsync(input);
+            // Select radio button
+            var radioLocator = radio switch
+            {
+                "Quote" => Quote_Radio,
+                "Claim" => Claim_Radio,
+                _ => Search_Radio
+            };
+            var radioButton = page.Locator(radioLocator);
+            await radioButton.CheckAsync();
+            // Click Continue
+            var continueBtn = page.Locator(Continue_Btn);
+            await continueBtn.ClickAsync();
+            await page.WaitForLoadStateAsync(LoadState.DOMContentLoaded);
+            // Click policy row
+            var policyClickLocator = string.Format(PolicyClick_Text, input);
+            var policyRow = page.Locator(policyClickLocator);
+            await policyRow.ClickAsync();
+            await page.WaitForLoadStateAsync(LoadState.DOMContentLoaded);
+        }
+    }
+}
---
+++ BDD.Playwright.Origami/Pages/AgentPages/Quote_LocationPage.cs
@@ -0,0 +1,73 @@
+﻿using BDD.Playwright.GBIZ.PageElements;
+using BDD.Playwright.GBIZ.Pages.CommonPage;
+using Microsoft.Playwright;
+using Reqnroll;
+
+namespace BDD.Playwright.Origami.Pages.AgentPages
+{
+    public class Quote_LocationPage : BasePage
+    {
+        public Quote_LocationPage(ScenarioContext scenarioContext) : base(scenarioContext) { }
+
+        #region Xpath
+        public string Addlocation { get; set; } = "//a[text()='Add Location']";
+        public string Protection_Drp { get; set; } = "//select[@name='loc_protection_1' and @id='fld_loc_protection_1']";
+        public string StreetAddress_input { get; set; } = "//input[@name='loc_streetAddress_1']";
+        public string City_input { get; set; } = "//input[@id='fld_loc_city_1']";
+        public string Zipcode_input { get; set; } = "//input[@id='fld_loc_zipCode_1']";
+        public string Milestofire_input { get; set; } = "//input[@id='fld_loc_milesToFireDept_1']";
+        public string RespondingFireDept_Input { get; set; } = "//input[@id='fld_loc_respondingFireDept_1']";
+        public string Loc_PassagewaysLit_Radio { get; set; } = "//input[contains(@id,'loc_passagewaysLit_1')  and @value='{0}']";
+        public string Loc_PorchHandrails_Radio { get; set; } = "//input[contains(@id,'loc_porchHandrails_1')  and @value='{0}']";
+        public string Loc_StairwaysHandrails_Radio { get; set; } = "//input[contains(@id,'loc_stairwaysHandrails_1')  and @value='{0}']";
+        public string Loc_RiInsuredWithin70MiOfLocation_1_Radio { get; set; } = "//input[contains(@id,'loc_riInsuredWithin70MiOfLocation_1')  and @value='{0}']";
+        public string Loc_Thereatrampoline_Radio { get; set; } = "//input[contains(@id,'loc_trampolineOrPool_1')  and @value='{0}']";
+        public string Continue_btn { get; set; } = "//button[contains(text(),'Continue')]";
+        #endregion
+
+        public async Task LocationDatafillAsync(dynamic commonFunctions)
+        {
+            await page.WaitForLoadStateAsync(LoadState.DOMContentLoaded);
+            await Button.ClickButtonAsync(Addlocation, ActionType.Click, true, 1);
+            await page.WaitForTimeoutAsync(3000);
+            await DropDown.SelectDropDownAsync(Protection_Drp, commonFunctions.LocationProtection_LabelAndValue.Item2, true, 1);
+            await InputField.SetTextAreaInputFieldAsync(StreetAddress_input, commonFunctions.LocationStreetAddress_LabelAndValue.Item2, true, 1);
+            await InputField.SetTextAreaInputFieldAsync(City_input, commonFunctions.LocationCity_LabelAndValue.Item2, true, 1);
+            await InputField.SetTextAreaInputFieldAsync(Zipcode_input, commonFunctions.LocationZipCode_LabelAndValue.Item2, true, 1);
+            await InputField.SetTextAreaInputFieldAsync(Milestofire_input, commonFunctions.LocationMiles_LabelAndValue.Item2, true, 1);
+            await InputField.SetTextAreaInputFieldAsync(RespondingFireDept_Input, commonFunctions.LocationRespondingFireDepartment_LabelAndValue.Item2, true, 1);
+            if (!string.IsNullOrEmpty(commonFunctions.Locationtrampoline_LabelAndValue.Item2))
+            {
+                var trampolineRadio = string.Format(Loc_Thereatrampoline_Radio, commonFunctions.Locationtrampoline_LabelAndValue.Item2);
+                await RadioButton.SelectRadioButtonAsync(trampolineRadio, commonFunctions.Locationtrampoline_LabelAndValue.Item2, true, 1);
+            }
+
+            if (!string.IsNullOrEmpty(commonFunctions.LocationArepassagewayswelllighted_LabelAndValue.Item2))
+            {
+                var passagewaysLitRadio = string.Format(Loc_PassagewaysLit_Radio, commonFunctions.LocationArepassagewayswelllighted_LabelAndValue.Item2);
+                await RadioButton.SelectRadioButtonAsync(passagewaysLitRadio, commonFunctions.LocationArepassagewayswelllighted_LabelAndValue.Item2, true, 1);
+            }
+
+            if (!string.IsNullOrEmpty(commonFunctions.LocationAreallporches_LabelAndValue.Item2))
+            {
+                var porchHandrailsRadio = string.Format(Loc_PorchHandrails_Radio, commonFunctions.LocationAreallporches_LabelAndValue.Item2);
+                await RadioButton.SelectRadioButtonAsync(porchHandrailsRadio, commonFunctions.LocationAreallporches_LabelAndValue.Item2, true, 1);
+            }
+
+            if (!string.IsNullOrEmpty(commonFunctions.LocationAreallinterior_LabelAndValue.Item2))
+            {
+                var stairwaysHandrailsRadio = string.Format(Loc_StairwaysHandrails_Radio, commonFunctions.LocationAreallinterior_LabelAndValue.Item2);
+                await RadioButton.SelectRadioButtonAsync(stairwaysHandrailsRadio, commonFunctions.LocationAreallinterior_LabelAndValue.Item2, true, 1);
+            }
+
+            if (!string.IsNullOrEmpty(commonFunctions.LocationDoestheinsuredlivewithin70miles_LabelAndValue.Item2))
+            {
+                var insuredWithin70MiRadio = string.Format(Loc_RiInsuredWithin70MiOfLocation_1_Radio, commonFunctions.LocationDoestheinsuredlivewithin70miles_LabelAndValue.Item2);
+                await RadioButton.SelectRadioButtonAsync(insuredWithin70MiRadio, commonFunctions.LocationDoestheinsuredlivewithin70miles_LabelAndValue.Item2, true, 1);
+            }
+
+            await Button.ClickButtonAsync(Continue_btn, ActionType.Click, true, 1);
+            await page.WaitForLoadStateAsync(LoadState.DOMContentLoaded);
+        }
+    }
+}
